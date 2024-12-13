db7b7b5ef357336ba9d7e675fac4bc66dfee149c
Thiaguin0102 <tfeitosa214@gmail.com>
2024-12-13 11:10:28 -0300

    Add files via upload

---
+++ sql2.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE produtos;
+
+USE produtos;
+
+CREATE TABLE produtos (
+id_Produtos INT AUTO_INCREMENT PRIMARY KEY,
+nome_produto VARCHAR(50) NOT NULL,
+qualtidade INT,
+preco DECIMAL(5,2)
+);
+
+INSERT INTO produtos (nome_produto, qualtidade, preco) VALUES
+('Carro de Brinquedo', 10, 100.00),
+('Boneco', 20, 150.00),
+('Mouse Sem Fio', 50, 35.75);
+
+SELECT * FROM produtos
